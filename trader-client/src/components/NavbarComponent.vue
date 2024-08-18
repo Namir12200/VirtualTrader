0d39b4c853c0c7ea2e7ba3bbfa6827c57dbc10b9
Namirul Islam <namir12200@gmail.com>
2024-08-19 01:00:50 +0600

    Navbar Added

---
+++ trader-client/src/components/NavbarComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+    <div class="navbar">
+        <nav>
+            <RouterLink class="nav-items" to="/">Home</RouterLink>
+            <RouterLink class="nav-items" to="/about">About</RouterLink>
+        </nav>
+    </div>
+</template>
